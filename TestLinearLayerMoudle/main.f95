869266739a1475016a3363879d0ffec5577c651e
liviurotiul <liviurotiultelefon@gmail.com>
2020-03-23 01:28:31 +0200

    managed to make a working funtional class, will start working on funtionalities

--- TestLinearLayerMoudle/main.f95
+++ TestLinearLayerMoudle/main.f95
@@ -1,27 +1,26 @@
 module linear_layer
 	implicit none
+	private
 	type, public :: linear_layer_object
-		real, dimension(:,:), private, allocatable :: W, B
+		!real, dimension(:,:), private, allocatable :: W, B
 		integer, dimension(2), private :: shape_W, shape_B
 	contains
-		procedure :: print_layer
-
+		procedure :: print_layer => print_layer_values
 	end type linear_layer_object
 contains
-	subroutine print_layer(this)
-		implicit none
-		integer :: x, y, i, j
-		print *, "Wheights"
-		x = this%shape_W(1)
-		y = this%shape_W(2)
-		do i=1, x
-			do j=1, y
-				write(*, fmt="(1x,f8.3)", advance = "no") this%(i,j)
-			end do
-			write(*,*)
-		end do
-	end subroutine print_layer
+	subroutine print_layer_values(this)
+		!implicit none
+		class(linear_layer_object), intent(in) :: this
+		print *, "sal"
+	end subroutine print_layer_values
 	!TO DO: make function that prints matrix for convenince
 	!TO DO: when making the actual module i shoul make a module for matrices as well (print and utilities)
 end module linear_layer
+
+program main
+	use linear_layer
+	implicit none
+	type(linear_layer_object) :: a
+	call a%print_layer
+end program main 
 
